696118829f90e1133957bfb217adf292238f3109
hansin <huxinghan@loonxi.com>
2020-06-01 10:19:13 +0800

    1

--- rust_test/src/main.rs
+++ rust_test/src/main.rs
@@ -11,4 +11,36 @@ fn main() {
 
     println!("The value of y is: {}", y);
     println!("The value of x is: {}", x);
+
+
+    // loop
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+          break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+
+    // while
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("[while] the value is: {}", a[index]);
+        index += 1;
+    }
+
+    // for
+    for element in a.iter() {
+      println!("[for] the value is: {}", element);
+    }
+
+    for number in 1..4 {
+      println!("{}!", number);
+    }
 }
