1d6fbb7c617bcb1dd2086651683d041e5ab5ec63
hansin <huxinghan@loonxi.com>
2020-07-15 10:51:13 +0800

    minigrep

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,4 +1,4 @@
-
+use std::env;
 use std::fs;
 use std::error::Error;
 
@@ -6,6 +6,7 @@ use std::error::Error;
 pub struct Config {
   pub query: String,
   pub filename: String,
+  pub case_sensitive: bool,
 }
 
 impl Config {
@@ -18,7 +19,14 @@ impl Config {
     let query = args[1].clone();
     let filename = args[2].clone();
 
-    Ok(Config { query, filename })
+    // CASE_INSENSITIVE=1 cargo run to poem.txt
+    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+    Ok(Config {
+      query,
+      filename,
+      case_sensitive,
+    })
   }
 }
 
@@ -26,11 +34,47 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   // let contents = fs::read_to_string(config.filename)
   //     .expect("Something went wrong reading the file");
   let contents = fs::read_to_string(config.filename)?;
-    
-  println!("With text:\n{}", contents);
+
+  let results = if config.case_sensitive {
+    search(&config.query, &contents)
+  } else {
+    search_case_insensitive(&config.query, &contents)
+  };
+
+  for line in results {
+    println!("{}", line);
+  }
 
   Ok(())
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.contains(query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
+// 不区分大小写
+pub fn search_case_insensitive<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
+  let query = query.to_lowercase();
+  let mut results = Vec::new();
+
+  for line in contents.lines() {
+    if line.to_lowercase().contains(&query) {
+      results.push(line);
+    }
+  }
+
+  results
+}
+
 
 // impl Config {
 //   fn new(args: &[String]) -> Config {
@@ -52,3 +96,34 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
 //   Config { query, filename }
 // }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,21 +3,22 @@ use std::process;
 
 use minigrep::Config;
 
+// CASE_INSENSITIVE=1 cargo run to poem.txt > output.txt
+
 fn main() {
     // 在迭代器上调用 collect 以将其转换为包含向量的集合
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-      println!("Problem parsing arguments: {}", err);
+      eprintln!("Problem parsing arguments: {}", err);
       process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.filename);
+    println!("Searching for {}, In file {}:", config.query, config.filename);
 
     // run(config);
     if let Err(e) = minigrep::run(config) {
-      println!("Application error: {}", e);
+      eprintln!("Application error: {}", e);
       process::exit(1);
     }
 }
